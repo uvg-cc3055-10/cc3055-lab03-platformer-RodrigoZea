ed627e695648986405d5d12da09c37c93253023b
Rodrigo Zea <zearodrigo37@gmail.com>
2018-04-18 07:55:46 -0600

    Agregando funcionalidades detalladas en la guía. Movimiento, sonido, etc.

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -7,6 +7,9 @@ public class Character : MonoBehaviour {
 
     Rigidbody2D rb2d;
     SpriteRenderer sr;
+    Animator anim;
+    public AudioClip jumpClip;
+    AudioSource src;
     public Camera cam;
     private float speed = 5f;
     private float jumpForce = 250f;
@@ -15,13 +18,16 @@ public class Character : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
+        src = GetComponent<AudioSource>();
         sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
         cam.transform.position = new Vector3(rb2d.transform.position.x, cam.transform.position.y, cam.transform.position.z);
     }
 	
 	// Update is called once per frame
 	void Update () {
         float move = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", Mathf.Abs(move));
         if (move != 0) {
             rb2d.transform.Translate(new Vector3(1, 0, 0) * move * speed * Time.deltaTime);
             cam.transform.position = new Vector3(rb2d.transform.position.x, cam.transform.position.y, cam.transform.position.z);
@@ -32,6 +38,8 @@ public class Character : MonoBehaviour {
 
         if (Input.GetButtonDown("Jump")) {
             rb2d.AddForce(Vector2.up*jumpForce);
+            src.clip = jumpClip;
+            src.Play();
         }
 
 	}
